8a7a7a28688613a707ca9cbd96ef2ee6b51d230f
ceciliedreyer <149535863+ceciliedreyer@users.noreply.github.com>
2024-10-27 21:15:41 +0100

    Updated HTML + CSS structure

--- src/Drum.js
+++ src/Drum.js
@@ -1,69 +1,22 @@
-import React, { useRef } from "react";
-import MIDISounds from "midi-sounds-react";
+import React from "react";
 import "./Drum.css";
 
 export default function Drum() {
   /**
-   * find a music api
+   * find a music library
    * The toggle is set to drums
    * if the toggle is = drums I return:
    *   Once I hit a button a pre-set sound is called
    * Once the toggle switches I update the state to piano
-   * if the toggle is = piano I return:
-   *   once I hit a button I call the API and get the piano sounds
    */
-  const midiSounds = useRef(null);
-
-  function handleNote(event) {
-    event.preventDefault();
-    midiSounds.current.playDrumsNow([2]);
-  }
 
   return (
     <div className="Drum">
       <div className="wrapper">
-        <div className="pad-bank">
-          <div className="row">
-            <div clasName="col">
-              <button
-                className="btn btn-primary shadow-sm p-3 m-1 rounded drum-pad"
-                onClick={handleNote}
-              >
-                Q
-              </button>
-
-              <button className="btn btn-primary shadow-sm p-3 m-1 rounded drum-pad">
-                W
-              </button>
-              <button className="btn btn-primary shadow-sm p-3 m-1 rounded drum-pad">
-                E
-              </button>
-            </div>
-            <div clasName="col">
-              <button className="btn btn-primary shadow-sm p-3 m-1 rounded drum-pad ">
-                A
-              </button>
-
-              <button className="btn btn-primary shadow-sm p-3 m-1 rounded drum-pad">
-                S
-              </button>
-              <button className="btn btn-primary shadow-sm p-3 m-1 rounded drum-pad">
-                D
-              </button>
-            </div>
-            <div clasName="col">
-              <button className="btn btn-primary shadow-sm p-3 m-1 rounded drum-pad ">
-                Z
-              </button>
-
-              <button className="btn btn-primary shadow-sm p-3 m-1 rounded drum-pad">
-                X
-              </button>
-              <button className="btn btn-primary shadow-sm p-3 m-1 rounded drum-pad">
-                C
-              </button>
-            </div>
-          </div>
+        <div clasName="pad-grid">
+          <button className="btn btn-primary shadow-sm p-3 m-1 rounded drum-pad">
+            Q
+          </button>
         </div>
         <div className="controls">
           <h1 className="power-switch">POWER</h1>
@@ -92,7 +45,6 @@ export default function Drum() {
           </div>
         </div>
       </div>
-      <MIDISounds ref={midiSounds} appElementName="root" />
     </div>
   );
 }
