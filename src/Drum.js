82e1976f6001b972540f20ece145187066f75f0c
ceciliedreyer <149535863+ceciliedreyer@users.noreply.github.com>
2024-10-28 10:57:41 +0100

    Added state management and handleFunctions

--- src/Drum.js
+++ src/Drum.js
@@ -3,22 +3,69 @@ import { soundBank1, soundBank2 } from "./Sounds";
 import "./Drum.css";
 
 export default function Drum() {
-  const [Power, setPower] = useState(true);
-  const [Bank, setBank] = useState(true); // true for soundBank1
+  const [power, setPower] = useState(true);
+  const [bank, setBank] = useState(true); // true for soundBank1
   const [display, setDisplay] = useState("");
   const [volume, setVolume] = useState(0.5);
+
+  let currentSounds;
+  if (bank === true) {
+    currentSounds = soundBank1;
+  } else {
+    currentSounds = soundBank2;
+  }
+
+  function handleBank() {
+    if (bank === true) {
+      setBank(false);
+      setDisplay("bank 1");
+    } else {
+      setBank(true);
+      setDisplay("bank 2");
+    }
+  }
+
+  function handlePower() {
+    setPower(!power);
+
+    if (power === true) {
+      setDisplay("ON");
+    } else {
+      setDisplay("");
+    }
+  }
+
+  function handleVolume(volume) {
+    setVolume(volume.target.value);
+    setDisplay(`volume: ${Math.round(volume.target.value)}`);
+  }
+
+  function startDrum(sound, label) {
+    if (power === true) {
+      const audio = new Audio(sound);
+      audio.volume = volume;
+      audio.play();
+      setDisplay(label);
+    }
+  }
 
   return (
     <div className="Drum">
       <div className="wrapper">
         <div clasName="pad-grid">
-          {sounds.map(function (sounds) {
+          {currentSounds.map(function (sounds) {
             return (
               <button
                 className="btn btn-primary shadow-sm p-3 m-1 rounded drum-pad"
+                onClick={function () {
+                  startDrum(sounds.sound, sounds.label);
+                }}
                 key={sounds.key}
+                sound={sounds.sound}
+                label={sounds.label}
               >
                 {sounds.key}
+                <audio id={sounds.key} src={sounds.sound} className="clip" />
               </button>
             );
           })}
@@ -31,13 +78,15 @@ export default function Drum() {
               type="checkbox"
               role="switch"
               id="flexSwitchCheckDefault"
+              onChange={handlePower}
             />
           </div>
-          <div className="btn btn-primary sound">Pad-Title</div>
+          <div className="btn btn-primary sound">{display}</div>
           <input
             type="range"
             className="form-range volume-slider"
             id="customRange1"
+            onChange={handleVolume}
           />
           <h1 className="bank-switch">BANK</h1>
           <div className="form-check form-switch">
@@ -46,6 +95,7 @@ export default function Drum() {
               type="checkbox"
               role="switch"
               id="flexSwitchCheckDefault"
+              onChange={handleBank}
             />
           </div>
         </div>
--- src/Sounds.js
+++ src/Sounds.js
@@ -1,1 +1,95 @@
-import React from "react";
+export const soundBank1 = [
+  {
+    key: "Q",
+    sound: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+    label: "Heater 1",
+  },
+  {
+    key: "W",
+    sound: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+    label: "Heater 2",
+  },
+  {
+    key: "E",
+    sound: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+    label: "Heater 3",
+  },
+  {
+    key: "A",
+    sound: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+    label: "Heater 4",
+  },
+  {
+    key: "S",
+    sound: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+    label: "Clap",
+  },
+  {
+    key: "D",
+    sound: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+    label: "Open HH",
+  },
+  {
+    key: "Z",
+    sound: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+    label: "Kick n’ Hat",
+  },
+  {
+    key: "X",
+    sound: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+    label: "Kick",
+  },
+  {
+    key: "C",
+    sound: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+    label: "Closed HH",
+  },
+];
+
+export const soundBank2 = [
+  {
+    key: "Q",
+    sound: "https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3",
+    label: "Chord 1",
+  },
+  {
+    key: "W",
+    sound: "https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3",
+    label: "Chord 2",
+  },
+  {
+    key: "E",
+    sound: "https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3",
+    label: "Chord 3",
+  },
+  {
+    key: "A",
+    sound: "https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3",
+    label: "Shaker",
+  },
+  {
+    key: "S",
+    sound: "https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3",
+    label: "Open HH",
+  },
+  {
+    key: "D",
+    sound: "https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3",
+    label: "Closed HH",
+  },
+  {
+    key: "Z",
+    sound: "https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3",
+    label: "Punchy Kick",
+  },
+  {
+    key: "X",
+    sound: "https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3",
+    label: "Side Stick",
+  },
+  {
+    key: "C",
+    sound: "https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3",
+    label: "Snare",
+  },
+];
